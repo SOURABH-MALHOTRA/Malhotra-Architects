5285417a020dd3f88c2a3181d7ab142b0e672e0c
SOURABH-MALHOTRA <malhotrasourabh034@gmail.com>
2025-01-25 23:07:35 +0530

    Blog Website

--- Backend/Middleware/accessToken.js
+++ Backend/Middleware/accessToken.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const accessToken = async (userId) => {
   const token = await jwt.sign({ id: userId }, process.env.SECRET_KEY, {
-    expiresIn: "30s",
+    expiresIn: "1h",
   });
 
   return token;
